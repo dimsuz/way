0c9d57bc60c6e1e86df3c5302867f3f1ddc98d3d
Dmitry Suzdalev <mail@dimsuz.ru>
2022-07-10 15:00:24 +0200

    Move template files out of source folder

--- way-sample-android/template/screen/source/{{^folder_name}}/{{^screen_name}}Ui.kt
+++ way-sample-android/template/screen/source/{{^folder_name}}/{{^screen_name}}Ui.kt
@@ -15,7 +15,10 @@ import ru.dimsuz.way.sample.android.ui.foundation.BaseUi
 @Composable
 fun {{^screen_name}}Ui(viewModel: {{^screen_name}}ViewModel) {
   BaseUi(viewModel = viewModel) { state ->
-    Column(verticalArrangement = Arrangement.spacedBy(16.dp).padding(24.dp)) {
+    Column(
+      modifier = Modifier.padding(24.dp),
+      verticalArrangement = Arrangement.spacedBy(16.dp),
+    ) {
       Text(state.title, color = contentColorFor(backgroundColor = state.color), modifier = Modifier.background(state.color).padding(16.dp))
       Button(onClick = viewModel::onContinue) {
         Text("Continue")
